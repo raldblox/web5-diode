b925c00079f9eff5415ad472306eae4d9c6ee673
raldblox <raldblox@outlook.com>
2023-11-27 19:06:25 +0800

    fix diodelink

--- src/components/utils/DiodeLink.js
+++ src/components/utils/DiodeLink.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import React, { useContext, useState } from 'react'
 
 const DiodeLink = () => {
-    const { setName, name, lockedName, connectAccount, connecting } = useContext(Context);
+    const { setName, name, lockedName, connectAccount, connecting, userDid } = useContext(Context);
 
     return (
         <div className='grid items-center w-full text-sm font-medium duration-150 border md:inline-flex md:w-fit md:gap-x-2 rounded-xl'>
@@ -11,7 +11,7 @@ const DiodeLink = () => {
                 <label>diode:</label>
                 <input disabled={lockedName} value={name || lockedName} onChange={(e) => setName(e.target.value)} className='w-fit bg-transparent max-w-[150px] h-full border-none outline-none' />
             </div>
-            {lockedName ?
+            {userDid ?
                 <Link href="/manage" className='flex items-center justify-center px-4 py-2 m-1 text-center uppercase md:whitespace-nowrap xbtn'>
                     Manage
                 </Link> :
